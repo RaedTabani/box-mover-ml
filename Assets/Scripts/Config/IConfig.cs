ba739a06e39adba193a3964b02b063815c32e118
Raed <raedtabanics@gmail.com>
2023-09-13 09:17:07 +0200

    Zenject_Timer

---
+++ Assets/Scripts/Config/IConfig.cs
@@ -0,0 +1,8 @@
+namespace Config{
+    public interface IConfig 
+    {
+        public float agentRunSpeed{get;}
+        public float agentRotationSpeed{get;}
+        public float spawnZoneMultiplier{get;} 
+    }
+}
---
+++ Assets/Scripts/Core/Timer.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using Config;
+using UnityEngine;
+using Zenject;
+
+namespace Core{
+    public class Timer : MonoBehaviour
+    {
+        private IConfig config;
+
+        [Inject]
+        public void Init(IConfig config){
+            this.config = config;
+            Debug.Log(config.agentRotationSpeed);
+        }
+    }
+}
